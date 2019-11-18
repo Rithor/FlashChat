4be36f6002556ab6a8e4f24a4bd9ed432b7bd479
Rithor <vitaliy.andrianov@icloud.com>
2019-11-18 14:15:27 +0300

    ititial commit; add .gitignore

---
+++ FlashChat/Controler/LaunchScreenVC.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenVC.swift
+//  FlashChat
+//
+//  Created by Vitalii on 18.11.2019.
+//  Copyright © 2019 Vitalii Andrianov. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
